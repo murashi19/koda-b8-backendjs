b6d01495bcecedb1afe9c05609081a3f4400624d
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-08-03 18:02:47 +0700

    feat(auth): add routes auth register

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
 
+import authRoute from "./routes/authRoute.js";
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/auth", authRoute);
 
 export default app;
