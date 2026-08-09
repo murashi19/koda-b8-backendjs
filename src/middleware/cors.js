3f56f0b063b7ab3234bf88f5d6f55e845fd6bf63
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-08-09 22:01:40 +0700

    fix: fixing url cors frontend

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,24 +1,35 @@
 import { constants } from "node:http2";
+
 /**
- *
  * @param {import("express").Request} req
  * @param {import("express").Response} res
  * @param {function()} next
  */
 
-// const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const ALLOWED_ORIGINS = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
 function corsMiddleware(req, res, next) {
-  // res.setHeader("Access-Control-Allow-Origin", FRONTEND_URL);
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  const origin = req.headers.origin;
+
+  if (origin && ALLOWED_ORIGINS.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS",
   );
+
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   if (req.method === "OPTIONS") {
     res.sendStatus(constants.HTTP_STATUS_NO_CONTENT);
     return;
   }
+
   next();
 }
 
