7fc1c8fe2a94c64f6b5a82202a06b1c5af825525
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-08-14 02:02:32 +0700

    chore: add file migration for add alter table public image

--- src/migrations/20260813190044-add-image-public-id-to-product-images.cjs
+++ src/migrations/20260813190044-add-image-public-id-to-product-images.cjs
@@ -3,12 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
     await queryInterface.addColumn("product_images", "image_public_id", {
       type: Sequelize.STRING(255),
       allowNull: true,
@@ -16,12 +10,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
     await queryInterface.removeColumn("product_images", "image_public_id");
   },
 };
