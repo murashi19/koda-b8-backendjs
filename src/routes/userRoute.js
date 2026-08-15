cf9039e559bb4ea4d1562dad1c727bf0ce3a200d
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-08-16 01:46:53 +0700

    feat: add CRUD user for Customer Management(admin)

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -5,9 +5,11 @@ import {
   UpdateUser,
 } from "../controllers/user.controller.js";
 import authMiddleware from "../middleware/auth.js";
+import requireAdmin from "../middleware/admin.js";
 
 const router = express.Router();
 router.use(authMiddleware);
+router.use(requireAdmin);
 
 /**
  * @openapi
